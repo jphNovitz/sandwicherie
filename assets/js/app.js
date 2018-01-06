2fb20cebcf7022e0e63188162c0fc0940c885a18
Jiphi <novitz@gmail.com>
2018-01-06 15:51:47 +0100

    form & field manipulations

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,6 +1,19 @@
 // button is the hamburger icon and menu is the lateral menu hidden by default
 const button = document.getElementById('menu');
 const menu = document.getElementById('lateral-menu');
+const test = document.getElementsByTagName('input');
+for (let i=0 ; i< test.length ; i++){
+    test[i].addEventListener("focusin", function (event) {
+        this.style.bottom = 0;
+    });
+    test[i].addEventListener("focusout", function (event) {
+        if (this.value){
+            this.style.bottom = 0;
+        }else {
+            this.style.bottom = "28px";
+        }
+    })
+}
 /*button.addEventListener("click", function(){
     menu.style.marginLeft = '0vw';
 });*/
