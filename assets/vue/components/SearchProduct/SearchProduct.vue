92d6780f0e660b1f104693b50f75074994cd6a85
Jiphi <novitz@gmail.com>
2018-04-30 22:20:28 +0200

    search product with typing name or description

---
+++ assets/vue/components/SearchProduct/SearchProduct.vue
@@ -0,0 +1,37 @@
+<template>
+<div>
+    <div class="emph"> Tu manges quoi à midi ?</div>
+    <sui-input placeholder="Search..." icon="search" v-model="typing" />
+    <div v-for="product in products">{{product.name}}</div>
+</div>
+</template>
+
+<script>
+    export default {
+        name: "SearchProduct",
+        data (){
+            return {
+                typing: ''
+            }
+        },
+        computed: {
+            products: function () {
+                let raw = document.getElementById('products').dataset.vars
+                let list = JSON.parse(raw).last
+                if (this.typing != '') {
+                    return list.filter(p => (
+                        (p.name.toLowerCase().indexOf(this.typing)) != -1) ||
+                        (p.description.toLowerCase().indexOf(this.typing) != -1)
+                    )
+                } else {
+                    return this.typing
+                }
+            }
+        }
+    }
+
+</script>
+
+<style scoped>
+
+</style>
