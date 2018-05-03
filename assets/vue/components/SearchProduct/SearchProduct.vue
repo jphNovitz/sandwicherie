4f427b5868c48e6f8b4ee37c8e73ec465df71660
Jiphi <novitz@gmail.com>
2018-05-03 20:58:51 +0200

    tempalte & reactivity in search product

--- assets/vue/components/SearchProduct/SearchProduct.vue
+++ assets/vue/components/SearchProduct/SearchProduct.vue
@@ -1,8 +1,16 @@
 <template>
 <div>
     <div class="emph"> Tu manges quoi à midi ?</div>
-    <sui-input placeholder="Search..." icon="search" v-model="typing" />
-    <div v-for="product in products">{{product.name}}</div>
+        <sui-input placeholder="Choisi ton repas " icon="search" v-model="typing" />
+    <div class="ui left aligned selection massive list container">
+        <div class="item" v-for="product in products">
+            <img class="ui avatar image" :src="'/images/products/'+product.images[0].imageName">
+            <div class="content">
+                <div class="header">{{product.name}}</div>
+                {{product.description}}
+            </div>
+     </div>
+    </div>
 </div>
 </template>
 
@@ -18,10 +26,11 @@
             products: function () {
                 let raw = document.getElementById('products').dataset.vars
                 let list = JSON.parse(raw).last
-                if (this.typing != '') {
+                if (this.typing !== '') {
                     return list.filter(p => (
-                        (p.name.toLowerCase().indexOf(this.typing)) != -1) ||
-                        (p.description.toLowerCase().indexOf(this.typing) != -1)
+                        (p.name.toLowerCase().indexOf(this.typing)) !== -1) ||
+                        (p.description.toLowerCase().indexOf(this.typing) !== -1)
+
                     )
                 } else {
                     return this.typing
