598bede927962dc70741d7c1fee8abb9b3f6f74d
Jiphi <novitz@gmail.com>
2018-06-19 22:52:18 +0200

    allergies

--- assets/vue/pages/products.vue
+++ assets/vue/pages/products.vue
@@ -1,22 +1,40 @@
 <template>
 <div>
-    <sui-menu secondary>
-        <sui-menu-item  link
-                       @click="selected=null"
-                       :class="{active: selected === null}">Tout</sui-menu-item>
-        <sui-menu-item link v-for="type in types"
-                       :key="type.slug"
-                       @click="selected=type.id"
-                       :class="{active: type.id === selected}">{{type.name}}
 
-        </sui-menu-item>
-    </sui-menu>
+    <sui-grid >
+        <sui-grid-row>
+            <sui-grid-column :width="2">
+                <h3>Allergies</h3>
+                <sui-list>
+                    <sui-list-item
+                            v-for="allergy in allergies"
+                            :key="allergy.id"
+                            @click="changeAllergies(allergy.id)">
+                        {{allergy.name}}
+                    </sui-list-item>
+                </sui-list>
+            </sui-grid-column>
+            <sui-grid-column :width="14">
+                <sui-menu secondary>
+                    <sui-menu-item  link
+                                    @click="selected=null"
+                                    :class="{active: selected === null}">Tout</sui-menu-item>
+                    <sui-menu-item link v-for="type in types"
+                                   :key="type.slug"
+                                   @click="selected=type.id"
+                                   :class="{active: type.id === selected}">{{type.name}}
 
-    <sui-card-group :items-per-row="3">
-       <card-product v-for="product in products"
-                     :product="product" :group="'products'">
-       </card-product>
-    </sui-card-group>
+                    </sui-menu-item>
+                </sui-menu>
+
+                <sui-card-group stackable :items-per-row="3">
+                   <card-product v-for="product in products"
+                                 :product="product" :group="'products'">
+                   </card-product>
+                </sui-card-group>
+            </sui-grid-column>
+        </sui-grid-row>
+    </sui-grid>
 </div>
 </template>
 
@@ -32,7 +50,8 @@
         data(){
             return {
                 activeType: '',
-                selected: null
+                selected: null,
+                allergiesList: []
             }
         },
         computed: {
@@ -55,8 +74,27 @@
                 return stack ;
             },
             types: function() {
-                return this.$store.getters.types
+                return this.$store.getters.types ;
+            },
+            allergies: function(){
+                return this.$store.getters.allergies;
+            },
+            blacklist: function(){
+                let filtered = this.allergies.filter(a =>{
+                    return  this.allergiesList.indexOf(a.id) > -1
+                })
+                let list =  []
+                filtered.map(fil => {
+                    fil.categories.map(cat =>{
+                        list.push(cat.slug)
+                    });
+                    fil.ingredients.map(ing =>{
+                        list.push(ing.slug)
+                    })
+                })
+                return list;
             }
+
         },
         methods: {
             return_name: function (elm) {
@@ -65,6 +103,14 @@
             },
             select: function (t) {
                 this.selection = t;
+            },
+            changeAllergies: function (a) {
+               let pos = this.allergiesList.indexOf(a);
+               if (pos === -1) {
+                   this.allergiesList.push(a)
+               } else {
+                   this.allergiesList.splice(pos,1)
+               }
             }
         }
     }
