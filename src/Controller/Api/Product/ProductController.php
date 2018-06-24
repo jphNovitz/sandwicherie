dce1ddb8e6f0be290713c39e802294ec3c969f8e
Jiphi <novitz@gmail.com>
2018-06-24 15:05:12 +0200

    test products

--- src/Controller/Api/Product/ProductController.php
+++ src/Controller/Api/Product/ProductController.php
@@ -29,6 +29,9 @@ class ProductController extends FOSRestController{
     public function getProducts()
     {
 
+        $test = $this->customLoader->CreateJsonResponse('App:Product');
+        die('hello world');
+        $this->customLoader->CreateJsonResponse('App:Product');
         return  $this->customLoader->CreateJsonResponse('App:Product');
     }
 
