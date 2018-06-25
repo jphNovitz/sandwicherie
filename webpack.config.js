543366ca12508d62dba3365b2577665777203a72
Jiphi <novitz@gmail.com>
2018-06-25 22:45:39 +0200

    .

--- webpack.config.js
+++ webpack.config.js
@@ -5,7 +5,7 @@ Encore
     // the project directory where compiled assets will be stored
     .setOutputPath('public/build/')
     // the public path used by the web server to access the previous directory
-    .setPublicPath('/public')
+    .setPublicPath('/build')
     .cleanupOutputBeforeBuild()
     .enableSourceMaps(!Encore.isProduction())
     // uncomment to create hashed filenames (e.g. app.abc123.css)
