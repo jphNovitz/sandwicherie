73de96e3ad58252ce0c3465ad6074d66cd0bc115
Jiphi <novitz@gmail.com>
2018-08-26 20:32:03 +0200

    new fonctionnality (user update)
    add missings fields ins userUpdate components
    add an event listener on sumit and a put request to the api
    new action in userController (api) -> decode deceived data and update user
    encode user and response status -> send request
    back in vuejs modify current objet in template

--- assets/vue/components/User/userDetails.vue
+++ assets/vue/components/User/userDetails.vue
@@ -1,30 +1,66 @@
 <template>
-    <div>
-        <sui-form>
-            <sui-form-field>
-                <label>Nom</label>
-                <input placeholder="Nom" >
-            </sui-form-field>
-            <sui-form-field>
-                <label>Prenom</label>
-                <input placeholder="Prenom" >
-            </sui-form-field>
-            <sui-button type="submit">Submit</sui-button>
-        </sui-form>
-    </div>
+        <sui-grid id="userDetail">
+            <sui-grid-row  :columns="2">
+                <sui-grid-column class="user_form" >
+                <sui-form @submit.prevent="updateUser">
+                    <sui-form-field>
+                        <label>Nom d'utilisateur</label>
+                        <input placeholder="Nom d'utilisateur"  v-model="user.username">
+                    </sui-form-field>
+                    <sui-form-field>
+                        <label>Email</label>
+                        <input placeholder="Email"  v-model="user.email">
+                    </sui-form-field>
+                    <sui-form-field>
+                        <label>Nom</label>
+                        <input placeholder="Nom"  v-model="user.last_name">
+                    </sui-form-field>
+                    <sui-form-field>
+                        <label>Prenom</label>
+                        <input placeholder="Prenom" v-model="user.first_name" >
+                    </sui-form-field>
+                    <label>Adresse </label>
+                    <sui-form-fields>
+                        <sui-input placeholder="Rue" v-model="user.street" />
+                        <sui-input placeholder="num" v-model="user.street_nr" class="ui one wide column" />
+                    </sui-form-fields>
+                    <sui-form-field>
+                        <label>Localité</label>
+                        <input placeholder="localité" v-model="user.city" >
+                    </sui-form-field>
+                    <sui-button type="submit">Submit</sui-button>
+                </sui-form>
+            </sui-grid-column>
+            </sui-grid-row>
+        </sui-grid>
 </template>
 <script>
     export default {
         name: 'userDetail',
         props: ['user'],
         data() {
-            return {
-
+            return {}
+        },
+        methods: {
+            updateUser: function () {
+                this.$http.put('/api/s/profile', JSON.stringify(this.user)).then(response => {
+                    this.user = response.body;
+                }, error => {
+                    console.log('error: ' + error.body)
+                });
             }
         }
     }
 
 </script>
-<style>
+<style scoped>
+
+    #userDetail{
+        text-align: left;
+    }
+
+    .user_form{
+        margin-left: 25%;
+    }
 
 </style>
--- src/Controller/Api/User/UserController.php
+++ src/Controller/Api/User/UserController.php
@@ -4,28 +4,34 @@ Namespace App\Controller\Api\User ;
 
 
 use App\Model\CustomObjectLoaderInterface;
+use App\Model\CustomPersisterInterface;
 use App\Service\CustomObjectLoader;
 use Doctrine\ORM\EntityManagerInterface;
+use FOS\RestBundle\Controller\Annotations as Rest;
 use FOS\RestBundle\Controller\Annotations\Get;
+use FOS\RestBundle\Controller\Annotations\Patch;
+use FOS\RestBundle\Controller\Annotations\Put;
 use FOS\RestBundle\Controller\FOSRestController;
 use Hateoas\Hateoas;
 use Hateoas\HateoasBuilder;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Security\Core\Authentication\Token\Storage\TokenStorageInterface;
-use Symfony\Component\Security\Core\Authentication\Token\TokenInterface;
 
 class UserController extends FOSRestController {
 
-    protected $userLoader;
+
     protected $objectLoader;
+    protected $persister;
     protected $em;
     protected $hateoas;
     protected $tokenStorage ;
 
-    public function __construct(CustomObjectLoaderInterface $objectLoader, TokenStorageInterface $tokenStorage )
+    public function __construct(CustomObjectLoaderInterface $objectLoader, TokenStorageInterface $tokenStorage, CustomPersisterInterface $persister )
     {
         $this->objectLoader = $objectLoader;
         $this->tokenStorage = $tokenStorage;
+        $this->persister = $persister;
     }
 
     /**
@@ -37,6 +43,34 @@ class UserController extends FOSRestController {
         $user = $this->tokenStorage->getToken()->getUser();
         $json = $hateoas->serialize($user,'json');
         $response = new Response($json, 200, array('application/json'));
+        $response->headers->set('Access-Control-Allow-Headers', 'origin, content-type, accept');
+        $response->headers->set('Access-Control-Allow-Origin', '*');
+        $response->headers->set('Access-Control-Allow-Methods', 'POST, GET, PUT, DELETE, PATCH, OPTIONS');
+        return $response;
+    }
+
+    /**
+     * @Put("s/profile", name="update_user")
+     */
+    public function updateUser(Request $request)
+    {
+        $user = $this->tokenStorage->getToken()->getUser();
+        $data = $request->getContent();
+        $user_received = $this->get('jms_serializer')->deserialize($data, 'App\Entity\User', 'json');
+        $user = $user_received;
+
+        $hateoas = HateoasBuilder::create()->build();
+
+        try {
+            $this->persister->update($user);
+            $json = $hateoas->serialize($user,'json');
+            $status = 200 ;
+        } catch (\Exception $e){
+            $json = $hateoas->serialize(['message'=>$e],'json');
+            $status = 500 ;
+        }
+
+        $response = new Response($json, $status, array('application/json'));
         $response->headers->set('Access-Control-Allow-Headers', 'origin, content-type, accept');
         $response->headers->set('Access-Control-Allow-Origin', '*');
         $response->headers->set('Access-Control-Allow-Methods', 'POST, GET, PUT, DELETE, PATCH, OPTIONS');
