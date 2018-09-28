55531340e33ff3d3c8a4143654e62c9d9612835a
Jiphi <novitz@gmail.com>
2018-09-28 19:47:10 +0200

    lateral menu refactor

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,69 +1,42 @@
+/*************************************************************/
+/** app.js used for glabal javascript                       **/
+/**                                                         **/
+/** part of laclementine project                            **/
+/** @author: Novitz Jean-Philippe  <novitz@gmail.com        **/
+/**                                                         **/
+/** 2018                                                    **/
+/*************************************************************/
+
 import Vue from 'vue';
 
 
 /******************/
 /** lateral menu **/
 /*****************/
+
 new Vue({
-    el: '#app',
+    el: '#lateral-menu',
     data: {
         show: false,
     },
     computed:    {
         button: function () {
-            return document.getElementById('button').firstChild
+            // return document.getElementById('general-button')
+            return document.querySelectorAll('#general-button')
+
         }
     },
     mounted() {
+
         const that = this
         document.addEventListener('click', (e) =>{
-            if (e.target === this.button){
-                this.show = !this.show
-            }  else if (this.show === true) {
-                this.show = !this.show
-            }
+            that.button.forEach(b=>{
+                if ((e.target === b) || (e.target === b.parentElement)) {
+                    this.show = !this.show
+                }else if (this.show === true) {
+                    this.show = !this.show
+                }
+            })
         })
     }
 });
-
-
-//
-// /*********************/
-// /** floating button **/
-// /*********************/
-//
-// Vue.config.delimiters = ['${', '}'];
-// const action = new Vue({
-//     el: '#action',
-//     data() {
-//         return {
-//             menuActive: 'notVisible',
-//             detailed: []
-//         }
-//     },
-//     methods: {
-//         showMenu: function () {
-//             if (this.menuActive === 'notVisible') {
-//                 this.menuActive = 'visible'
-//             } else {
-//                 this.menuActive = 'notVisible'
-//             }
-//         },
-//         toggleDetail: function (id) {
-//             let i = this.detailed.indexOf(id);
-//             if (i > 0) {
-//                 this.detailed.splice(i, 1)
-//             } else {
-//                 this.detailed.push(id);
-//             }
-//             console.log(this.detailed)
-//         },
-//         detailClass: function (id) {
-//             if (this.detailed.indexOf(id) > -1) {
-//                 return 'down';
-//             } else {
-//                 return 'right';
-//             }
-//         }
-//     }
-// })
