70bbbf68952fccbfe9b2b5a579eeb7f79da01730
NOVITZ Jean-Philippe <novitz@gmail.com>
2019-03-26 23:25:26 +0100

    a bit on the new version

---
+++ assets/nuxt/components/Footer.vue
@@ -0,0 +1,81 @@
+<template>
+  <div id="footer" class="ui three column stackable grid">
+    <div class="ui orange row">
+      <div class="column center aligned">
+        <h4 class="header">{{ infos.title }}</h4>
+        <p>
+          {{ infos.subTitle }} <br />
+          TVA: {{ infos.tva }}
+        </p>
+      </div>
+      <div class="column center aligned">
+        <h4 class="header">Notre adresse</h4>
+        <p>
+          {{ infos.adress }} <br />
+          {{ infos.city }}
+        </p>
+        <p>
+          {{ infos.phone }} <br />
+          {{ infos.email }}
+        </p>
+      </div>
+      <div class="column center aligned">
+        <h4 class="header">Réseaux sociaux</h4>
+        <a
+          :href="infos.socialFacebook"
+          class="ui circular facebook icon button"
+        >
+          <i class="facebook icon"></i>
+        </a>
+        <a
+          :href="infos.socialInstagram"
+          class="ui circular instagram icon button"
+        >
+          <i class="instagram icon"></i>
+        </a>
+        <a
+          :href="infos.socialInstagram"
+          class="ui circular linkedin icon button"
+        >
+          <i class="linkedin icon"></i>
+        </a>
+      </div>
+    </div>
+  </div>
+</template>
+<script>
+export default {
+  components: {},
+  computed: {
+    infos: function() {
+      console.log(this.$store.getters['init/infos'])
+      return this.$store.getters['init/infos']
+    }
+  },
+  mounted() {
+    /* this.$axios
+      .$get('http://localhost:8000/api/privacy/public/infos')
+      .then(function(response) {
+        // handle success
+        console.log(JSON.parse(response))
+      }) */
+  },
+  methods: {}
+}
+</script>
+<style scoped>
+#footer {
+  margin: 2rem 0 0 0 !important;
+  font-size: 0.75rem;
+}
+
+.orange {
+  border-top: solid 2px green;
+}
+
+@media screen and (min-width: 1024px) {
+  #footer {
+    font-size: 0.9rem;
+  }
+}
+</style>
---
+++ assets/nuxt/components/Header.vue
@@ -0,0 +1,55 @@
+<template>
+  <header class="sixteen wide column ui fluid container">
+    <div class="ui grid">
+      <div class="orange row">
+        <div class="two wide column">
+          <i class="white bars big link icon" @click="clickMenu"></i>
+        </div>
+
+        <div class="eight wide column mobile only">
+          <img
+            src="./../assets/logo_white-400.png"
+            class="ui mini inline image"
+          />
+        </div>
+        <div class="eight wide column computer only tablet only">
+          <img
+            src="./../assets/logo_white-400.png"
+            class="ui tiny inline image"
+          />
+          <span class="ui white medium header">
+            .: Sanwicherie :.
+          </span>
+        </div>
+        <div
+          class="right floated right aligned three wide computer six wide mobile column"
+        >
+          <i class="large right user icon"></i>
+          <i class="large shopping cart link icon"></i>
+        </div>
+      </div>
+    </div>
+  </header>
+</template>
+
+<script>
+export default {
+  components: {},
+  methods: {
+    clickMenu: function() {
+      this.$emit('opensesame')
+    }
+  }
+}
+</script>
+
+<style scopped>
+header {
+  margin-left: 0 !important;
+  margin-right: 0 !important;
+}
+
+.white {
+  color: #fff !important;
+}
+</style>
