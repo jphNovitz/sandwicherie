78fbf5292962903adc57dd855d2f4b850144cb70
NOVITZ Jean-Philippe <novitz@gmail.com>
2019-04-24 22:16:24 +0200

    instagram init

---
+++ assets/nuxt/instagram.vue
@@ -0,0 +1,20 @@
+<template>
+  <div>
+    instagram
+  </div>
+</template>
+<script>
+export default {
+  data() {
+    return {}
+  },
+  async fetch({ app, store }) {
+    const response = await app.$axios.get(store.getters['instagram/url'], {
+      params: {
+        access_token: store.getters['instagram/access_token']
+      }
+    })
+    store.commit('init/SET_INFOS', response.data)
+  }
+}
+</script>
