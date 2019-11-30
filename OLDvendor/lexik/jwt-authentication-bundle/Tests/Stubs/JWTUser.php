d64e7f1f5e8d18ccabd0bc2312864230a82d76f4
NOVITZ Jean-Philippe <novitz@gmail.com>
2019-11-30 23:53:27 +0100

    150 deprecation correction

--- OLDvendor/lexik/jwt-authentication-bundle/Tests/Stubs/JWTUser.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-namespace Lexik\Bundle\JWTAuthenticationBundle\Tests\Stubs;
-
-use Lexik\Bundle\JWTAuthenticationBundle\Security\User\JWTUser as BaseUser;
-
-final class JWTUser extends BaseUser
-{
-}
